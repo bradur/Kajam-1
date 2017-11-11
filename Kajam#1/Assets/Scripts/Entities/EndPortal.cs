6708910287497eacb18f37e058391aaef1ff7eae
Juho Saarelainen <bradurdung@gmail.com>
2017-11-11 09:23:49 +0200

    Portal pulls you in but lets you go.

--- Kajam#1/Assets/Scripts/Entities/EndPortal.cs
+++ Kajam#1/Assets/Scripts/Entities/EndPortal.cs
@@ -5,14 +5,18 @@
 using UnityEngine;
 using System.Collections;
 
-public class EndPortal : MonoBehaviour {
+public class EndPortal : MonoBehaviour
+{
 
     private Rigidbody2D playerRigidbody2D;
     private float myMass = 75;
 
-    void Start () {
+    void Start()
+    {
         focusCamera = Camera.main.GetComponent<FocusAndZoomInTarget>();
         playerRigidbody2D = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        originalAngularDrag = playerRigidbody2D.angularDrag;
+        originalGravityScale = playerRigidbody2D.gravityScale;
     }
 
     private bool isTeleporting = false;
@@ -23,16 +27,25 @@ public class EndPortal : MonoBehaviour {
 
     private float pullSpeed = 5f;
     private float zPosition = 0f;
+    private float originalAngularDrag;
+    private float originalGravityScale;
 
-    void Update () {
-        if (hasHit && !ProjectileManager.main.CurrentProjectileIsAlive() && KeyManager.main.GetKeyDown(Action.Teleport))
+    void Update()
+    {
+        if (hasHit && !isTeleporting)
         {
-            focusCamera.Init(transform);
-            isTeleporting = true;
-            zPosition = playerRigidbody2D.transform.position.z;
-        }
-        if (isTeleporting)
-        {
+            if (Vector2.Distance(transform.position, playerRigidbody2D.transform.position) > 1.1f)
+            {
+                ResetPlayer();
+            }
+            else if (KeyManager.main.GetKeyDown(Action.Teleport) && !ProjectileManager.main.CurrentProjectileIsAlive())
+            {
+
+                focusCamera.Init(transform);
+                isTeleporting = true;
+                zPosition = playerRigidbody2D.transform.position.z;
+            }
+        } else if (isTeleporting) { 
             Vector3 playerPosition = Vector3.Lerp(playerRigidbody2D.transform.position, transform.position, pullSpeed * Time.unscaledDeltaTime);
             zPosition += Time.unscaledDeltaTime * zSpeed;
             playerPosition.z = zPosition;
@@ -47,10 +60,28 @@ public class EndPortal : MonoBehaviour {
     {
         if (collider.gameObject.tag == "Player")
         {
-            Vector2 force = transform.position - collider.gameObject.transform.position;
-            force.Normalize();
-            playerRigidbody2D.AddForce(force * playerRigidbody2D.mass * myMass / force.magnitude);
+            if (Vector2.Distance(transform.position, collider.gameObject.transform.position) < 0.05f)
+            {
+                playerRigidbody2D.transform.position = Vector2.Lerp(playerRigidbody2D.transform.position, transform.position, Time.unscaledDeltaTime * pullSpeed);
+                playerRigidbody2D.gravityScale = 0f;
+            }
+            else
+            {
+                Vector2 force = transform.position - collider.gameObject.transform.position;
+                force.Normalize();
+                playerRigidbody2D.AddForce(force * playerRigidbody2D.mass * myMass / force.magnitude);
+            }
         }
+    }
+
+    private void ResetPlayer()
+    {
+        Debug.Log("RESET!");
+        hasHit = false;
+        isTeleporting = false;
+        zPosition = 0f;
+        playerRigidbody2D.angularDrag = originalAngularDrag;
+        playerRigidbody2D.gravityScale = originalGravityScale;
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
@@ -59,14 +90,7 @@ public class EndPortal : MonoBehaviour {
         {
             hasHit = true;
             ProjectileManager.main.KillCurrentProjectile();
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collider)
-    {
-        if (collider.gameObject.tag == "Player" && hasHit)
-        {
-            hasHit = false;
+            playerRigidbody2D.angularDrag = 1f;
         }
     }
 }
